284084d65ec97fdc03154b99e17f799a2e8c03a5
Rheykynen <e.heine210@gmail.com>
2025-12-01 13:49:36 +0100

    added character counter function in stats.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
-
 def main():
     book_path = "books/frankenstein.txt"
     content= get_book_text(book_path)
     counter = count_words(content)
+    print(f"Found {counter} total words")
+    character_dict = count_characters(content)
+    print(character_dict)
     #sort_counter(counter)
 
 main()
--- stats.py
+++ stats.py
@@ -3,4 +3,16 @@ def count_words(content):
     words = content.split()
     for word in words: 
         num_words += 1
-    print(f"Found {num_words} total words")
+    return num_words
+
+def count_characters(content):
+    character_count = {}
+    words = content.split()
+    for word in words:
+        for character in word:
+            character = character.lower()
+            if character not in character_count:
+                character_count[character] = 1
+            else:
+                character_count[character] += 1
+    return character_count
