084884a5df39abca4aaa0557912424b6ec020bb3
Rheykynen <e.heine210@gmail.com>
2025-12-03 14:46:34 +0100

    addes sys.argv with books, finalized

--- main.py
+++ main.py
@@ -7,22 +7,21 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     content = get_book_text(book_path)
     counter = count_words(content)
     character_dict = count_characters(content)
     sorted_list = dict_into_sort_list(character_dict)
-    print(f"""
-============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
------------ Word Count ----------)
-Found {counter} total words
---------- Character Count -------
-"""
-)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {counter} total words")
+    print("--------- Character Count -------")
+
     for item in sorted_list:
-        if item["char"].isalpha() == True:
+        if item["char"].isalpha():
             print(f"{item['char']}: {item['num']}")
+
     print("============= END ===============")
 
 main()
